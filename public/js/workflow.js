ecd11fe73cbc0d9a27e3ae4085aa4a0d8b78d413
wallydrag <debashish.ghatak@gmail.com>
2020-07-16 10:25:10 +0530

    show timecheck

--- public/js/workflow.js
+++ public/js/workflow.js
@@ -1,81 +1,85 @@
 (function () {
-    var workflow =
-        {
-            "title": "Zoom Demo",
-            "topics": [
-                {
-                    "title": "Overview",
-                    "ttl": "30",
-                    "links": [
-                        "https://marketplace.zoom.us/develop/create?source=devdocs"
-                    ]
+  var workflow =
+    {
+      "title": "Zoom Demo",
+      "topics": [
+	{
+	  "title": "Overview",
+	  "ttl": "20",
+	  "links": [
+	    "https://marketplace.zoom.us/develop/create?source=devdocs"
+	  ]
 
-                },
-                {
-                    "title": "Checklist Overview",
-                    "ttl": "30",
-                    "links": [
-                    ]
+	},
+	{
+	  "title": "Checklist Overview",
+	  "ttl": "20",
+	  "links": [
+	  ]
 
-                },
-                {
-                    "title": "Links",
-                    "ttl": "30",
-                    "links": [
-                        "https://marketplace.zoom.us/develop/create?source=devdocs",
-                    ]
+	},
+	{
+	  "title": "Links",
+	  "ttl": "10",
+	  "links": [
+	    "https://marketplace.zoom.us/develop/create?source=devdocs",
+	  ]
 
-                },
-                {
-                    "title": "QnA",
-                    "ttl": "3000",
-                    "links": [
-                        "https://marketplace.zoom.us/develop/create?source=devdocs",
-                    ]
+	},
+	{
+	  "title": "QnA",
+	  "ttl": "30",
+	  "links": [
+	    "https://marketplace.zoom.us/develop/create?source=devdocs",
+	  ]
 
-                }
-            ]
-        };
-
-
-    $("#overlay-add-agenda").click(function() {
-
-        $("#overlay-add-agenda").hide();
-
-        var checklistHTML = '<h2>' + workflow.title + ' </h2>';
-
-        for(var i =0;i <workflow.topics.length; i++) {
-            var topic = workflow.topics[i];
-            var topicID = "topic-" + i;
-            checklistHTML+= '<div class="checklist-item"> <input type="checkbox"  id="' + topicID + '"> ' + topic.title + ' (' + topic.ttl + ') </div>';
-        }
+	}
+      ]
+    };
 
 
+  $("#overlay-add-agenda").click(function() {
 
-        $("#overlay-checklist").html(checklistHTML);
+    $("#overlay-add-agenda").hide();
 
-        var currentTopicID = 0;
-        var startTime = new Date().getTime();
+    var checklistHTML = '<h2>' + workflow.title + ' </h2>';
+
+    for(var i =0;i <workflow.topics.length; i++) {
+      var topic = workflow.topics[i];
+      var topicID = "topic-" + i;
+      checklistHTML+= '<div class="checklist-item"> <input type="checkbox"  id="' + topicID + '"> ' + topic.title + ' (' + topic.ttl + ') </div>';
+    }
+
+    $("#overlay-checklist").html(checklistHTML);
+
+    var currentTopicID = 0;
+    var startTime = new Date().getTime();
+    $("#topic-" + currentTopicID).parent().addClass("checklist-highlight");
+
+    console.log(workflow.topics[currentTopicID].title);
+
+    setInterval(function(){
+      var timeElapsed = new Date().getTime() - startTime;
+      var topicTime = workflow.topics[currentTopicID].ttl * 1000;
+      if(timeElapsed > topicTime) {
+
+	$("#topic-" + currentTopicID).prop("checked", true);
+	$("#topic-" + currentTopicID).parent().removeClass("checklist-highlight");
+
+	startTime = new Date().getTime();
+	currentTopicID += 1;
 	$("#topic-" + currentTopicID).parent().addClass("checklist-highlight");
+	console.log(workflow.topics[currentTopicID].title)
+      }
 
-        console.log(workflow.topics[currentTopicID].title);
+      if (timeElapsed > topicTime/2 ) {
+	$("#overlay-timecheck").show();
+      } else {
+	$("#overlay-timecheck").hide();
+      }
+    }, 1000);
 
-
-        setInterval(function(){
-            var timeElapsed = new Date().getTime() - startTime;
-            if(timeElapsed > (workflow.topics[currentTopicID].ttl * 1000)) {
-
-                $("#topic-" + currentTopicID).prop("checked", true);
-		$("#topic-" + currentTopicID).parent().removeClass("checklist-highlight");
-
-                startTime = new Date().getTime();
-                currentTopicID += 1;
-		$("#topic-" + currentTopicID).parent().addClass("checklist-highlight");
-                console.log(workflow.topics[currentTopicID].title)
-            }
-        }, 1000);
-
-    });
+  });
 
 
 })();
